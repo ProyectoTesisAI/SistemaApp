f7c16cb10e0716e977fb5a4d8fca5c9a67dabf6f
xavy1693 <javier.reyes@epn.edu.ec>
2019-10-01 09:52:51 -0500

    Javier 01/10/2019 09:53
    -Falta de edicion la eliminacion de las imagenes ya existentes en la base
    -Se añadio los modulos de creacion de un nuevo informe desde un taller, donde se guarda la asistencia, un informe y  sus imagenes
    -Sus IMAGENES aun no se guardan, debido a que hay que reestructurar el codigo para guardar las imagenenes

--- app/src/main/java/ec/edu/epn/snai/Controlador/Adaptador/IngresarRegistroFotograficoAdaptador.java
+++ app/src/main/java/ec/edu/epn/snai/Controlador/Adaptador/IngresarRegistroFotograficoAdaptador.java
@@ -4,6 +4,7 @@ import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,7 +23,6 @@ import static ec.edu.epn.snai.R.id.btn_quitar;
 public class IngresarRegistroFotograficoAdaptador extends RecyclerView.Adapter<IngresarRegistroFotograficoAdaptador.RegistroFotograficoViewHolder> {
 
     private List<RegistroFotografico> fotografias = new ArrayList<>();
-    private OnItemClickListener mListener;
 
     public IngresarRegistroFotograficoAdaptador(List<RegistroFotografico> fotografias) {
         this.fotografias = fotografias;
@@ -32,18 +32,31 @@ public class IngresarRegistroFotograficoAdaptador extends RecyclerView.Adapter<I
     @Override
     public RegistroFotograficoViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.activity_item_editar_registro_fotografico, viewGroup, false);
-        RegistroFotograficoViewHolder holder = new RegistroFotograficoViewHolder(view,null);
+        RegistroFotograficoViewHolder holder = new RegistroFotograficoViewHolder(view);
         return holder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RegistroFotograficoViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull final RegistroFotograficoViewHolder viewHolder, int i) {
 
         byte[] byteDatos = Base64.decode(fotografias.get(i).getImagenAux(), Base64.DEFAULT);
         fotografias.get(i).setFoto(BitmapFactory.decodeByteArray(byteDatos, 0, byteDatos.length));
         //Seteo los valores en los diferentes controles
         viewHolder.imgImagen.setImageBitmap(fotografias.get(i).getFoto());
-        viewHolder.setOnClickListeners();
+        //viewHolder.setOnClickListeners();
+        viewHolder.btnQuitar.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                final int posicion=viewHolder.getLayoutPosition();
+                try{
+                    fotografias.remove(posicion);
+                    notifyDataSetChanged();
+                }catch (Exception e){
+                    Log.e(e.getMessage(),"Ocurrio un error en onClick, en OnBindViewHolder");
+                }
+            }
+        });
     }
 
     @Override
@@ -56,48 +69,10 @@ public class IngresarRegistroFotograficoAdaptador extends RecyclerView.Adapter<I
         ImageView imgImagen;
         Button btnQuitar;
 
-        public RegistroFotograficoViewHolder(final View itemView, final OnItemClickListener listener) {
+        public RegistroFotograficoViewHolder(final View itemView) {
             super(itemView);
             imgImagen = itemView.findViewById(R.id.imgRegistroFotograficoEditar);
             btnQuitar = itemView.findViewById(btn_quitar);
-
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (listener != null){
-                        int position = getAdapterPosition();
-                        System.out.println(position);
-                        if(position != RecyclerView.NO_POSITION){
-                            listener.onDeleteClick(position);
-                            notifyDataSetChanged();
-                        }
-                    }
-                }
-            });
         }
-        void setOnClickListeners() {
-            btnQuitar.setOnClickListener(new View.OnClickListener() {
-
-                @Override
-                public void onClick(View v) {
-                    for(int i=0;i<fotografias.size();i++){
-                        if(fotografias.get(i).getIdRegistroFotografico()!=null){
-                            fotografias.remove(i);
-                            notifyDataSetChanged();
-                            break;
-                        }
-                    }
-                }
-            });
-
-        }
-    }
-
-    public interface OnItemClickListener extends View.OnClickListener {
-        void onDeleteClick(int position);
-    }
-
-    public void setOnItemClickListener(View.OnClickListener listener) {
-        mListener = (OnItemClickListener) listener;
     }
 }
